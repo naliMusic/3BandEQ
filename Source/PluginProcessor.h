2d0ba5687dbd081a08194b25fa89a0d96ea7472f
MNguyen <nali.minh.ng@gmail.com>
2022-04-28 11:04:21 -0700

    Refactor low cut filter code

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -91,6 +91,57 @@ private:
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
     static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(ChainType& leftLowCut,
+                            const CoefficientType& cutCoffs,
+                            const Slope& lowCutSlope)
+    {
+        leftLowCut.template setBypassed<0>(true);
+        leftLowCut.template setBypassed<1>(true);
+        leftLowCut.template setBypassed<2>(true);
+        leftLowCut.template setBypassed<3>(true);
+
+        switch (lowCutSlope)
+        {
+        case Slope_12:
+        {
+            *leftLowCut.template get<0>().coefficients = *cutCoffs[0];
+            leftLowCut.template setBypassed<0>(false);
+            break;
+        }
+        case Slope_24:
+        {
+            *leftLowCut.template get<0>().coefficients = *cutCoffs[0];
+            leftLowCut.template setBypassed<0>(false);
+            *leftLowCut.template get<1>().coefficients = *cutCoffs[1];
+            leftLowCut.template setBypassed<1>(false);
+            break;
+        }
+        case Slope_36:
+        {
+            *leftLowCut.template get<0>().coefficients = *cutCoffs[0];
+            leftLowCut.template setBypassed<0>(false);
+            *leftLowCut.template get<1>().coefficients = *cutCoffs[1];
+            leftLowCut.template setBypassed<1>(false);
+            *leftLowCut.template get<2>().coefficients = *cutCoffs[2];
+            leftLowCut.template setBypassed<2>(false);
+            break;
+        }
+        case Slope_48:
+        {
+            *leftLowCut.template get<0>().coefficients = *cutCoffs[0];
+            leftLowCut.template setBypassed<0>(false);
+            *leftLowCut.template get<1>().coefficients = *cutCoffs[1];
+            leftLowCut.template setBypassed<1>(false);
+            *leftLowCut.template get<2>().coefficients = *cutCoffs[2];
+            leftLowCut.template setBypassed<2>(false);
+            *leftLowCut.template get<3>().coefficients = *cutCoffs[3];
+            leftLowCut.template setBypassed<3>(false);
+            break;
+        }
+        }
+    }
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (_3BandEQAudioProcessor)
 };
